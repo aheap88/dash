0810e67d1cc3c4aca85f794a3341e1503557fe1b
aheap88 <alexheap1988@gmail.com>
2015-10-14 23:07:01 +0100

    New upload

--- scripts/general.js
+++ scripts/general.js
@@ -8,7 +8,20 @@ $(document).ready(function(){
 			$(".sidebar").show();
 		}
 	};
+	$("a.mobile").click(function(){
+		$(".menu").slideToggle('fast');
+	});
 
 
+	window.onresize = function(event){
+		if($(window).width() > 320) {
+			$(".menu").show();
+		}
+	};
 
+	window.onresize = function(event){
+		if($(window).width() < 320) {
+			$(".menu").hide();
+		}
+	};
 });
